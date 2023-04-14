7e5248a8aba07c82cf9b03bb5fab37820092efe3
ZeusBiel <127859799+ZeusBiel@users.noreply.github.com>
2023-04-14 17:59:15 -0300

    v0.2

--- web-development-with-javascript/script.js
+++ web-development-with-javascript/script.js
@@ -4,3 +4,16 @@ function Enviar() {
     let idade = document.getElementsByName("idadeS")[0].value;
     Validar(nome, email, idade);
 }
+
+function Validar(nome, email, idade) {
+    if (nome === "") {
+        alert("Por favor, insira seu nome.");
+        return;
+    }
+    else if (!/^[A-Za-z]+$/.test(nome)) {
+        alert("Por favor, insira um nome válido.");
+        return;
+    }
+    else if (email === "") {
+        alert("Por favor, insira seu email.");
+        return;
